7468f8f203eea1ec0eddb22ae154c1aa342d1c29
Domin-MND <dominus.the.creator@gmail.com>
2023-08-18 11:27:15 +0300

    refactor: less hardcoded

--- panda.config.ts
+++ panda.config.ts
@@ -1,5 +1,35 @@
 import { defineConfig } from "@pandacss/dev";
 
+function hslToHsla(opacity: number, hsl: string) {
+  return hsl.replace("hsl", "hsla").slice(0, -1) + `, ${opacity / 100})`;
+}
+
+interface OpacityColor {
+  value: {
+    _dark: { value: string };
+    _light: { value: string };
+  };
+}
+
+function generateOpacities(
+  lightHsl: string,
+  darkHsl: string = lightHsl,
+  opacities: number[] = [5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+) {
+  const result: Record<number, OpacityColor> = {};
+
+  for (let i in opacities) {
+    result[opacities[i]] = {
+      value: {
+        _dark: { value: hslToHsla(opacities[i], darkHsl) },
+        _light: { value: hslToHsla(opacities[i], lightHsl) },
+      },
+    };
+  }
+
+  return result;
+}
+
 export default defineConfig({
   // Whether to use css reset
   preflight: true,
@@ -21,131 +51,23 @@ export default defineConfig({
   // Useful for theme customization
   theme: {
     extend: {},
-
-    // Semantic tokens used in css, non semantic aren't used
     semanticTokens: {
       colors: {
-        text: {
-          value: {
-            _dark: { value: "{colors.white}" },
-            _light: { value: "{colors.black}" },
-          },
-        },
-        background: {
-          value: {
-            _dark: { value: "{colors.black}" },
-            _light: { value: "{colors.white}" },
-          },
-        },
-        accent: {
-          value: {
-            _dark: { value: "{colors.ceil}" },
-            _light: { value: "{colors.blurple}" },
-          },
-        },
-        primary: { value: "{colors.dbh}" },
-        secondary: {
-          value: {
-            _dark: { value: "{colors.eerie}" },
-            _light: { value: "{colors.azure}" },
-          },
-        }
+        pillbackground: generateOpacities("hsl(0, 0%, 2%)", "hsl(0, 0%, 98%)"),
+        background: generateOpacities("hsl(0, 0%, 98%)", "hsl(0, 0%, 2%)"),
+        text: generateOpacities("hsl(0, 0%, 2%)", "hsl(0, 0%, 98%)"),
+        accent: generateOpacities(
+          "hsl(225.9, 46.7%, 50%)",
+          "hsl(226.5, 46.4%, 70%)"
+        ),
+        primary: generateOpacities("hsl(225.7, 58%, 64.5%)"),
+        secondary: generateOpacities(
+          "hsl(224.3, 45.1%, 90%)",
+          "hsl(224.3, 45.1%, 10%)"
+        ),
       },
     },
     tokens: {
-      colors: {
-        black: {
-          5: { value: "hsla(0, 0%, 2%, 0.05)" },
-          10: { value: "hsla(0, 0%, 2%, 0.1)" },
-          20: { value: "hsla(0, 0%, 2%, 0.2)" },
-          30: { value: "hsla(0, 0%, 2%, 0.3)" },
-          40: { value: "hsla(0, 0%, 2%, 0.4)" },
-          50: { value: "hsla(0, 0%, 2%, 0.5)" },
-          60: { value: "hsla(0, 0%, 2%, 0.6)" },
-          70: { value: "hsla(0, 0%, 2%, 0.7)" },
-          80: { value: "hsla(0, 0%, 2%, 0.8)" },
-          90: { value: "hsla(0, 0%, 2%, 0.9)" },
-          100: { value: "hsl(0, 0%, 2%)" },
-        },
-        white: {
-          5: { value: "hsla(0, 0%, 98%, 0.05)" },
-          10: { value: "hsla(0, 0%, 98%, 0.1)" },
-          20: { value: "hsla(0, 0%, 98%, 0.2)" },
-          30: { value: "hsla(0, 0%, 98%, 0.3)" },
-          40: { value: "hsla(0, 0%, 98%, 0.4)" },
-          50: { value: "hsla(0, 0%, 98%, 0.5)" },
-          60: { value: "hsla(0, 0%, 98%, 0.6)" },
-          70: { value: "hsla(0, 0%, 98%, 0.7)" },
-          80: { value: "hsla(0, 0%, 98%, 0.8)" },
-          90: { value: "hsla(0, 0%, 98%, 0.9)" },
-          100: { value: "hsl(0, 0%, 98%)" },
-        },
-        blurple: {
-          5: { value: "hsla(225.9, 46.7%, 50%, 0.05)" },
-          10: { value: "hsla(225.9, 46.7%, 50%, 0.1)" },
-          20: { value: "hsla(225.9, 46.7%, 50%, 0.2)" },
-          30: { value: "hsla(225.9, 46.7%, 50%, 0.3)" },
-          40: { value: "hsla(225.9, 46.7%, 50%, 0.4)" },
-          50: { value: "hsla(225.9, 46.7%, 50%, 0.5)" },
-          60: { value: "hsla(225.9, 46.7%, 50%, 0.6)" },
-          70: { value: "hsla(225.9, 46.7%, 50%, 0.7)" },
-          80: { value: "hsla(225.9, 46.7%, 50%, 0.8)" },
-          90: { value: "hsla(225.9, 46.7%, 50%, 0.9)" },
-          100: { value: "hsl(225.9, 46.7%, 50%)" },
-        },
-        ceil: {
-          5: { value: "hsla(226.5, 46.4%, 70%, 0.05)" },
-          10: { value: "hsla(226.5, 46.4%, 70%, 0.1)" },
-          20: { value: "hsla(226.5, 46.4%, 70%, 0.2)" },
-          30: { value: "hsla(226.5, 46.4%, 70%, 0.3)" },
-          40: { value: "hsla(226.5, 46.4%, 70%, 0.4)" },
-          50: { value: "hsla(226.5, 46.4%, 70%, 0.5)" },
-          60: { value: "hsla(226.5, 46.4%, 70%, 0.6)" },
-          70: { value: "hsla(226.5, 46.4%, 70%, 0.7)" },
-          80: { value: "hsla(226.5, 46.4%, 70%, 0.8)" },
-          90: { value: "hsla(226.5, 46.4%, 70%, 0.9)" },
-          100: { value: "hsl(226.5, 46.4%, 70%)" },
-        },
-        dbh: {
-          5: { value: "hsla(225.7, 58%, 64.5%, 0.05)" },
-          10: { value: "hsla(225.7, 58%, 64.5%, 0.1)" },
-          20: { value: "hsla(225.7, 58%, 64.5%, 0.2)" },
-          30: { value: "hsla(225.7, 58%, 64.5%, 0.3)" },
-          40: { value: "hsla(225.7, 58%, 64.5%, 0.4)" },
-          50: { value: "hsla(225.7, 58%, 64.5%, 0.5)" },
-          60: { value: "hsla(225.7, 58%, 64.5%, 0.6)" },
-          70: { value: "hsla(225.7, 58%, 64.5%, 0.7)" },
-          80: { value: "hsla(225.7, 58%, 64.5%, 0.8)" },
-          90: { value: "hsla(225.7, 58%, 64.5%, 0.9)" },
-          100: { value: "hsl(225.7, 58%, 64.5%)" },
-        },
-        azure: {
-          5: { value: "hsla(224.3, 45.1%, 90%, 0.05)" },
-          10: { value: "hsla(224.3, 45.1%, 90%, 0.1)" },
-          20: { value: "hsla(224.3, 45.1%, 90%, 0.2)" },
-          30: { value: "hsla(224.3, 45.1%, 90%, 0.3)" },
-          40: { value: "hsla(224.3, 45.1%, 90%, 0.4)" },
-          50: { value: "hsla(224.3, 45.1%, 90%, 0.5)" },
-          60: { value: "hsla(224.3, 45.1%, 90%, 0.6)" },
-          70: { value: "hsla(224.3, 45.1%, 90%, 0.7)" },
-          80: { value: "hsla(224.3, 45.1%, 90%, 0.8)" },
-          90: { value: "hsla(224.3, 45.1%, 90%, 0.9)" },
-          100: { value: "hsl(224.3, 45.1%, 90%)" },
-        },
-        eerie: {
-          5: { value: "hsla(224.3, 45.1%, 10%, 0.05)" },
-          10: { value: "hsla(224.3, 45.1%, 10%, 0.1)" },
-          20: { value: "hsla(224.3, 45.1%, 10%, 0.2)" },
-          30: { value: "hsla(224.3, 45.1%, 10%, 0.3)" },
-          40: { value: "hsla(224.3, 45.1%, 10%, 0.4)" },
-          50: { value: "hsla(224.3, 45.1%, 10%, 0.5)" },
-          60: { value: "hsla(224.3, 45.1%, 10%, 0.6)" },
-          70: { value: "hsla(224.3, 45.1%, 10%, 0.7)" },
-          80: { value: "hsla(224.3, 45.1%, 10%, 0.8)" },
-          90: { value: "hsla(224.3, 45.1%, 10%, 0.9)" },
-          100: { value: "hsl(224.3, 45.1%, 10%)" },
-        },
-      },
       fonts: {
         body: { value: "system-ui, sans-serif" },
       },
