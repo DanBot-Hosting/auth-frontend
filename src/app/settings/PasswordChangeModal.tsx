e2c95190b8e15b4209563643a933329bd475fcbe
Domin-MND <dominus.the.creator@gmail.com>
2023-08-24 11:55:49 +0300

    feat!: static ui

---
+++ src/app/settings/PasswordChangeModal.tsx
@@ -0,0 +1,84 @@
+import { Input } from "@/components/Input";
+import { ModalProps } from "@/components/Modal";
+import { useModal } from "@/hooks/useModal";
+import { css } from "@styles/css";
+
+const modalConfirm = css({
+  color: "text.100!",
+
+  _hover: {
+    color: "text.100!",
+    textDecorationColor: "text.100!",
+  },
+});
+
+const modalFields = css({
+  display: "flex",
+  p: "0.625rem 0.75rem",
+  flexDir: "column",
+  justifyContent: "center",
+  alignItems: "flex-start",
+  gap: "1.25rem",
+  alignSelf: "stretch",
+});
+
+const modalField = css({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "flex-start",
+  gap: "0.625rem",
+  alignSelf: "stretch",
+
+  "& > input": {
+    maxW: "100%",
+    borderRadius: "0.625rem",
+  },
+});
+
+const modalLabel = css({
+  color: "text.90",
+  fontSize: "1rem",
+  fontWeight: "400",
+});
+
+export function usePasswordChangeModal() {
+  const { show: showModal, hide } = useModal();
+
+  const modalConfig: ModalProps = {
+    label: "Update your password",
+    description: "Enter your current password and a new password",
+    buttons: [
+      <button key="cancel" onClick={hide}>
+        Cancel
+      </button>,
+      <button key="update" className={modalConfirm} onClick={hide}>
+        Change my password!
+      </button>,
+    ],
+    children: (
+      <div className={modalFields}>
+        <div className={modalField}>
+          <span className={modalLabel}>Current password</span>
+          <Input placeholder="" />
+        </div>
+        <div className={modalField}>
+          <span className={modalLabel}>New password</span>
+          <Input placeholder="" />
+        </div>
+        <div className={modalField}>
+          <span className={modalLabel}>Confirm new password</span>
+          <Input placeholder="" />
+        </div>
+      </div>
+    ),
+  };
+
+  function show() {
+    showModal(modalConfig);
+  }
+
+  return {
+    show, hide
+  }
+}
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,94 +1,119 @@
 "use client";
-import { Input } from "@/components/Input";
-import { ModalProps } from "@/components/Modal";
-import { useModal } from "@/hooks/useModal";
+import { Avatar } from "@/components/Avatar";
 import { css } from "@styles/css";
+import { usePasswordChangeModal } from "./PasswordChangeModal";
+import { Input } from "@/components/Input";
+
+const main = css({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "1.875rem",
+  alignSelf: "stretch",
+});
 
 const button = css({
   cursor: "pointer",
-  color: "text.60",
-  transition: "color .3s ease-in-out",
+  alignSelf: "stretch",
+
+  fontWeight: "400",
+  fontSize: "1rem",
+  color: "text.90",
+  textDecoration: "underline",
+  textDecorationColor: "text.30",
+  textUnderlineOffset: "0.1875rem",
+  textDecorationThickness: "0.078125rem",
+  transition: "all 0.2s ease-in-out",
 
   _hover: {
-    color: "text.90",
-    transition: "color .3s ease-in-out",
-  },
-});
-
-const confirm = css({
-  color: "text.100!",
-
-  _hover: {
-    color: "text.100!",
-    textDecorationColor: "text.100!",
+    textDecorationColor: "text.100",
+    textDecorationThickness: "0.09375rem",
+    transition: "all 0.2s ease-in-out",
   },
 });
 
 const fields = css({
   display: "flex",
-  p: "0.625rem 0.75rem",
+  flexDirection: "column",
+  alignItems: "center",
+  gap: "2.8125rem",
+});
+
+const section = css({
+  display: "flex",
   flexDir: "column",
-  justifyContent: "center",
-  alignItems: "flex-start",
-  gap: "1.25rem",
-  alignSelf: "stretch",
+  gap: "0.9375rem",
 });
 
 const field = css({
   display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "flex-start",
-  gap: "0.625rem",
-  alignSelf: "stretch",
-
-  "& > input": {
-    maxW: "100%",
-    borderRadius: "0.625rem",
-  }
+  justifyContent: "flex-end",
+  alignItems: "center",
+  gap: "1.875rem",
+  width: "100%",
 });
 
 const label = css({
   color: "text.90",
-  fontSize: "1rem",
-  fontWeight: "400",
+  textAlign: "right",
+  fontSize: "1.125rem",
+  fontWeight: "300",
+});
+
+const input = css({
+  display: "flex",
+  width: "31.25rem",
+  minH: "3.125rem",
+  gap: "0.9375rem",
+
+  "& input": {
+    flex: "1 0 0",
+    maxW: "100%",
+  },
 });
 
 export default function Settings() {
-  const { show: showModal, hide: hideModal } = useModal();
-
-  const modalConfig: ModalProps = {
-    label: "Update your password",
-    description: "Enter your current password and a new password",
-    buttons: [
-      <button key="cancel" onClick={hideModal}>
-        Cancel
-      </button>,
-      <button key="update" className={confirm} onClick={hideModal}>
-        Change my password!
-      </button>,
-    ],
-    children: (
-      <div className={fields}>
-        <div className={field}>
-          <span className={label}>Current password</span>
-          <Input placeholder="" />
-        </div>
-        <div className={field}>
-          <span className={label}>New password</span>
-          <Input placeholder="" />
-        </div>
-        <div className={field}>
-          <span className={label}>Confirm new password</span>
-          <Input placeholder="" />
-        </div>
-      </div>
-    )
-  }
+  const { show: showModal } = usePasswordChangeModal();
 
   return (
-    <>
-      <button className={button} onClick={() => showModal(modalConfig)}>Use Password Change Modal</button>
-    </>
+    <div className={main}>
+      <Avatar
+        size={120}
+        src="https://avatars.githubusercontent.com/u/69919939"
+        alt="domin"
+        configurable
+      />
+      <div className={fields}>
+        <div className={section} id="public">
+          <div className={field}>
+            <label className={label}>Username</label>
+            <div className={input}>
+              <Input placeholder="" />
+            </div>
+          </div>
+          <div className={field}>
+            <label className={label}>Name</label>
+            <div className={input}>
+              <Input placeholder="First Name" />
+              <Input placeholder="Last Name" />
+            </div>
+          </div>
+        </div>
+        <div className={section} id="private">
+          <div className={field}>
+            <label className={label}>Email</label>
+            <div className={input}>
+              <Input placeholder="" />
+            </div>
+          </div>
+          <div className={field}>
+            <label className={label}>Password</label>
+            <div className={input}>
+              <button className={button} onClick={() => showModal()}>Change your password</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
