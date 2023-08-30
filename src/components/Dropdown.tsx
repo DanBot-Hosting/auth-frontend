859bd4b1d131928c975fe67f215c779924a340f7
Domin-MND <69919939+domin-mnd@users.noreply.github.com>
2023-08-30 16:45:12 +0300

    feat!: rewrite dropdown & segmentedcontrol to hook

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -2,8 +2,8 @@
 import { css } from "@styles/css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
 import { useCallback, useEffect, useRef, useState } from "react";
+import { useHoverable } from "@/hooks/useHoverable";
 
 const dropdown = css({
   display: "flex",
@@ -39,57 +39,21 @@ const option = css({
     transition: "color 0.2s ease-in-out",
   },
 
-  "&[data-active]": {
+  "&[data-active-hoverable]": {
     color: "text.90",
     transition: "color 0.2s ease-in-out",
   },
 });
 
-const focusDiv = css({
+const hoverable = css({
   position: "absolute",
   top: "0",
   left: "0",
   borderRadius: "0.625rem",
   bg: "pillbackground.70",
   zIndex: "3",
+  transition: "all 0.2s ease-in-out",
 });
-
-/**
- * Returns the inner space of an HTMLDivElement element.
- *
- * @param {HTMLDivElement} element - The HTMLDivElement element to calculate the inner space of.
- * @returns {number} The inner space of the HTMLDivElement element in pixels.
- */
-function getElementInnerSpace(element: HTMLDivElement): number {
-  const computed = getComputedStyle(element);
-  let elementSpace = element.clientWidth;
-  elementSpace -=
-    parseFloat(computed.paddingLeft) + parseFloat(computed.paddingRight);
-
-  return elementSpace;
-}
-
-/**
- * Returns the top offset of the inner content of the specified HTMLDivElement element.
- *
- * @param {HTMLDivElement} element - The HTMLDivElement element to get the top offset from.
- * @returns {number} The top offset of the inner content of the element in pixels.
- */
-function getElementInnerTopOffset(element: HTMLDivElement): number {
-  const computed = getComputedStyle(element);
-  return parseFloat(computed.paddingTop);
-}
-
-/**
- * Returns the left offset of the inner content of the specified HTMLDivElement element.
- *
- * @param {HTMLDivElement} element - The HTMLDivElement element to get the left offset from.
- * @returns {number} The left offset of the inner content of the element in pixels.
- */
-function getElementInnerLeftOffset(element: HTMLDivElement): number {
-  const computed = getComputedStyle(element);
-  return parseFloat(computed.paddingLeft);
-}
 
 /**
  * A Dropdown menu with a list of links and buttons to interact with.
@@ -100,90 +64,44 @@ function getElementInnerLeftOffset(element: HTMLDivElement): number {
  * @returns {JSX.Element} The rendered Dropdown component.
  */
 export function Dropdown({ links, onTabClick }: DropdownProps) {
-  const parentRef = useRef<HTMLDivElement | null>(null);
   const linksRef = useRef<(HTMLAnchorElement | null)[]>([]);
+  const [hoverableElement, setHoverableElement] =
+    useState<HTMLDivElement | null>(null);
   const pathname = usePathname();
-  const [position, setPosition] = useState({
-    width: 0,
-    height: 0,
-    // translation X and Y
-    translation: [0, 0],
-    visible: false,
+  const { find, change, hide, set } = useHoverable({
+    children: linksRef.current,
+    hoverable: hoverableElement,
   });
 
-  /** Resets the position of the hoverable. */
-  const resetPosition = useCallback(() => {
-    setPosition({
-      width: getElementInnerSpace(parentRef.current as HTMLDivElement),
-      height: 0,
-      translation: [
-        getElementInnerLeftOffset(parentRef.current as HTMLDivElement),
-        getElementInnerTopOffset(parentRef.current as HTMLDivElement),
-      ],
-      visible: false,
-    });
-  }, []);
-
-  /**
-   * Updates the position of the hoverable depending on the element.
-   *
-   * @param {HTMLAnchorElement} element - The element to update the position to.
-   */
-  const changePosition = useCallback((element: HTMLAnchorElement) => {
-    setPosition({
-      width: element?.offsetWidth ?? 0,
-      height: element?.offsetHeight ?? 0,
-      translation: [element?.offsetLeft ?? 0, element?.offsetTop ?? 0],
-      visible: true,
-    });
-  }, []);
-
-  /** Returns the position of the hoverable. */
-  const returnPosition = useCallback(() => {
+  useEffect(() => {
     const element = linksRef.current.find(
       (link) => link?.pathname === pathname
     );
 
-    if (!element) return resetPosition();
+    if (!element) return hide();
+    change(element);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname]);
 
-    for (let link in linksRef.current) {
-      linksRef.current[link]?.removeAttribute("data-active");
-    }
-
-    element.dataset.active = "true";
-    changePosition(element);
-  }, [changePosition, pathname, resetPosition]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(returnPosition, [pathname]);
+  const manage = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      if (onTabClick) onTabClick(event);
+      change(event.currentTarget);
+    },
+    [change, onTabClick]
+  );
 
   return (
-    <div className={dropdown} ref={parentRef}>
-      <motion.div
-        transition={{
-          type: "spring",
-          damping: 100,
-          stiffness: 1000,
-        }}
-        initial={{
-          display: position.visible ? "block" : "none",
-        }}
-        animate={{
-          x: position.translation[0],
-          y: position.translation[1],
-          width: position.width,
-          height: position.height,
-        }}
-        className={focusDiv}
-      />
+    <div className={dropdown}>
+      <div ref={setHoverableElement} className={hoverable} />
       {links.map((link, i) => (
         <Link
           key={i}
           href={link.link}
-          onClick={onTabClick}
+          onClick={manage}
           ref={(ref) => linksRef.current.push(ref)}
-          onMouseEnter={(event) => changePosition(event.currentTarget)}
-          onMouseOut={returnPosition}
+          onMouseEnter={(event) => set(event.currentTarget)}
+          onMouseOut={find}
           className={option}
         >
           {link.label}
--- src/components/SegmentedControl.tsx
+++ src/components/SegmentedControl.tsx
@@ -2,8 +2,9 @@
 import { css } from "@styles/css";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { motion } from "framer-motion";
 import { useCallback, useEffect, useRef, useState } from "react";
+import { useHoverable } from "@/hooks/useHoverable";
+import { flush } from "@/utils/css";
 
 const dropdown = css({
   display: "inline-flex",
@@ -38,57 +39,21 @@ const option = css({
     transition: "color 0.2s ease-in-out",
   },
 
-  "&[data-active]": {
+  "&[data-active-hoverable]": {
     color: "text.90",
     transition: "color 0.2s ease-in-out",
   },
 });
 
-const focusDiv = css({
+const hoverable = css({
   position: "absolute",
   top: "0",
   left: "0",
   borderRadius: "0.625rem",
   bg: "pillbackground.70",
   zIndex: "3",
+  transition: "all 0.2s ease-in-out",
 });
-
-/**
- * Returns the inner space of an HTMLDivElement element.
- *
- * @param {HTMLDivElement} element - The HTMLDivElement element to calculate the inner space of.
- * @returns {number} The inner space of the HTMLDivElement element in pixels.
- */
-function getElementInnerSpace(element: HTMLDivElement): number {
-  const computed = getComputedStyle(element);
-  let elementSpace = element.clientHeight;
-  elementSpace -=
-    parseFloat(computed.paddingTop) + parseFloat(computed.paddingBottom);
-
-  return elementSpace;
-}
-
-/**
- * Returns the top offset of the inner content of the specified HTMLDivElement element.
- *
- * @param {HTMLDivElement} element - The HTMLDivElement element to get the top offset from.
- * @returns {number} The top offset of the inner content of the element in pixels.
- */
-function getElementInnerTopOffset(element: HTMLDivElement): number {
-  const computed = getComputedStyle(element);
-  return parseFloat(computed.paddingTop);
-}
-
-/**
- * Returns the left offset of the inner content of the specified HTMLDivElement element.
- *
- * @param {HTMLDivElement} element - The HTMLDivElement element to get the left offset from.
- * @returns {number} The left offset of the inner content of the element in pixels.
- */
-function getElementInnerLeftOffset(element: HTMLDivElement): number {
-  const computed = getComputedStyle(element);
-  return parseFloat(computed.paddingLeft);
-}
 
 /**
  * A SegmentedControl pill with a list of links and buttons to interact with.
@@ -99,90 +64,50 @@ function getElementInnerLeftOffset(element: HTMLDivElement): number {
  * @returns {JSX.Element} The rendered SegmentedControl component.
  */
 export function SegmentedControl({ links, onTabClick }: DropdownProps) {
-  const parentRef = useRef<HTMLDivElement | null>(null);
   const linksRef = useRef<(HTMLAnchorElement | null)[]>([]);
+  const [hoverableElement, setHoverableElement] =
+    useState<HTMLDivElement | null>(null);
   const pathname = usePathname();
-  const [position, setPosition] = useState({
-    width: 0,
-    height: 0,
-    // translation X and Y
-    translation: [0, 0],
-    visible: false,
+  const { find, change, hide, set } = useHoverable({
+    children: linksRef.current,
+    hoverable: hoverableElement,
   });
 
-  /** Resets the position of the hoverable. */
-  const resetPosition = useCallback(() => {
-    setPosition({
-      width: 0,
-      height: getElementInnerSpace(parentRef.current as HTMLDivElement),
-      translation: [
-        getElementInnerLeftOffset(parentRef.current as HTMLDivElement),
-        getElementInnerTopOffset(parentRef.current as HTMLDivElement),
-      ],
-      visible: false,
-    });
-  }, []);
-
-  /**
-   * Updates the position of the hoverable depending on the element.
-   *
-   * @param {HTMLAnchorElement} element - The element to update the position to.
-   */
-  const changePosition = useCallback((element: HTMLAnchorElement) => {
-    setPosition({
-      width: element?.offsetWidth ?? 0,
-      height: element?.offsetHeight ?? 0,
-      translation: [element?.offsetLeft ?? 0, element?.offsetTop ?? 0],
-      visible: true,
-    });
-  }, []);
-
-  /** Returns the position of the hoverable. */
-  const returnPosition = useCallback(() => {
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => {
     const element = linksRef.current.find(
       (link) => link?.pathname === pathname
     );
 
-    if (!element) return resetPosition();
+    if (!element) return hide();
 
-    for (let link in linksRef.current) {
-      linksRef.current[link]?.removeAttribute("data-active");
-    }
+    if (!hoverableElement) return;
+    // Transition will be cleared on set call so it's alright
+    hoverableElement.style.transition = "none";
+    change(element);
+    flush(hoverableElement);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pathname, hoverableElement]);
 
-    element.dataset.active = "true";
-    changePosition(element);
-  }, [changePosition, pathname, resetPosition]);
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  useEffect(returnPosition, [pathname]);
+  const manage = useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => {
+      if (onTabClick) onTabClick(event);
+      change(event.currentTarget);
+    },
+    [change, onTabClick]
+  );
 
   return (
-    <div className={dropdown} ref={parentRef}>
-      <motion.div
-        transition={{
-          type: "spring",
-          damping: 100,
-          stiffness: 1000,
-        }}
-        initial={{
-          display: position.visible ? "block" : "none",
-        }}
-        animate={{
-          x: position.translation[0],
-          y: position.translation[1],
-          width: position.width,
-          height: position.height,
-        }}
-        className={focusDiv}
-      />
+    <div className={dropdown}>
+      <div ref={setHoverableElement} className={hoverable} />
       {links.map((link, i) => (
         <Link
           key={i}
           href={link.link}
-          onClick={onTabClick}
+          onClick={manage}
           ref={(ref) => linksRef.current.push(ref)}
-          onMouseEnter={(event) => changePosition(event.currentTarget)}
-          onMouseOut={returnPosition}
+          onMouseEnter={(event) => set(event.currentTarget)}
+          onMouseOut={find}
           className={option}
         >
           {link.label}
