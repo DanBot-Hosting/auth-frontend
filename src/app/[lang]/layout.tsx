ca6ab670fda4bc2d29b2f2bb08ca2b717bd6c29d
Domin-MND <69919939+domin-mnd@users.noreply.github.com>
2023-09-16 20:36:53 +0300

    fix: locale favicon

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,40 +1,40 @@
 import "./global.css";
 import type { Metadata } from "next";
 import { Layout } from "@/components/Layout";
-import { translate, locale } from "@/utils/dictionary";
+import { translate, locale, prependLocale } from "@/utils/dictionary";
 
 export const metadata: Metadata = {
   title: "DanBot Hosting",
   description: "Your free & fast hosting provider",
   icons: {
     apple: {
-      url: "/favicon/apple-touch-icon.png",
+      url: prependLocale("/favicon/apple-touch-icon.png"),
       sizes: "180x180",
     },
-    shortcut: "/favicon/favicon.ico",
+    shortcut: prependLocale("/favicon/favicon.ico"),
     icon: [
       {
-        url: "/favicon/favicon-32x32.png",
+        url: prependLocale("/favicon/favicon-32x32.png"),
         sizes: "32x32",
         type: "image/png",
       },
       {
-        url: "/favicon/favicon-16x16.png",
+        url: prependLocale("/favicon/favicon-16x16.png"),
         sizes: "16x16",
         type: "image/png",
       },
     ],
     other: {
-      url: "/favicon/safari-pinned-tab.svg",
+      url: prependLocale("/favicon/safari-pinned-tab.svg"),
       rel: "mask-icon",
       color: "#050505",
     },
   },
-  manifest: "/favicon/site.webmanifest",
+  manifest: prependLocale("/favicon/site.webmanifest"),
   themeColor: "#050505",
   other: {
     "msapplication-TileColor": "#050505",
-    "msapplication-config": "/favicon/browserconfig.xml",
+    "msapplication-config": prependLocale("/favicon/browserconfig.xml"),
   },
 };
 
